95cd81f1f7868eea2a4d3681baafd4eabf117d5f
Michal <mszyszka223@gmail.com>
2018-08-23 14:46:58 +0200

    DOM training

---
+++ Working on DOM elements/script.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('#theTitle');
+title.textContent = 'Ale jaja, zmieniam text z JS"em ';
+
+
+const beachImg = document.querySelector('img');
+
+console.log(beachImg.getAttribute('alt'));
